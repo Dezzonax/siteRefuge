4606db0b86c928cc45462cd335302032e366242a
Adrian <dezzonax@outlook.fr>
2024-10-01 01:52:04 +0200

    refonte infos adoptions & chiens caté

--- infos-adoption.php
+++ infos-adoption.php
@@ -13,35 +13,22 @@
 
             <h1>Infos Adoption</h1><hr><br>
 
-            <p>Frais d'adoption à compter du 1er janvier 2023 :</p>
+            <h4>CONDITIONS D’ADOPTION</h4>
+
             <ul>
-                <li>Chien ou chiot mâle : 250 €</li>
-                <li>Chien ou chiot femelle : 300 €</li>
-                <li>Chien mâle ou femelle de plus de 8 ans : 150 €</li>
-                <li>Chien mâle ou femelle de plus de 10 ans : Don libre</li>
+                <li>Être majeur</li>
+                <li>Présenter une pièce d’identité</li>
+                <li>Présenter un justificatif de domicile de moins de 3 mois</li>
+                <li>Une bonne adoption prend du temps, prévoir 1/2 heure à 1 heure en tenant compte de l’heure de fermeture du refuge</li>
             </ul>
 
-            <p>A savoir : les chiens de plus de 10 ans bénéficient de l'opération doyen de 30 Millions d'Amis soit une prise en charge des soins vétérinaire à hauteur de 600 €</p>
-            <ul>
-                <li>Chat ou chaton mâle : 180 €</li>
-                <li>Chat ou chaton femelle : 210 €</li>
-                <li>Chat mâle ou femelle de plus de 12 ans : Don libre</li>
-                <li>Lapin ou furet : 100 €</li>
-            </ul>
+            <p style="color: red;">Une pré-visite est effectuée au domicile du futur adoptant avant chaque adoption, afin de vérifier les conditions d’accueil, le futur mode de vie et la compatibilité de nos animaux avec leur nouvelle famille.</p>
 
-            <p>Ces frais comprennent l'identification, la vaccination, la stérilisation et le déparasitage de l'animal.<br><br>
-
-            Justificatifs à fournir pour toute adoption :</p>
+            <p>En application de la loi sur la maltraitance animal du 30 novembre 2021, les conditions d’adoption d’animaux , que ce soit dans les élevages, animaleries ou refuges sont modifiées.</p>
+            <p>Avant toute adoption, il est dorénavant demandé aux adoptants :</p>
             <ul>
-                <li>Pièce d'identité</li>
-                <li>Justificatif de domicile de moins de 3 mois</li>
-            </ul>
-
-            <p>En application de la loi sur la maltraitance animale du 30 novembre 2021, les conditions d'adoption d'animaux, que ce soit dans les élevages, animaleries ou refuges sont modifiés.<br>
-            Il est dorénavant demandé aux futurs adoptants :</p> 
-            <ul>
-                <li>De prendre connaissance et de signer un certificat d'engagement et de connaissance des besoins physiologiques, comportementaux, médicaux, financiers et logistiques de l'animal (certificats en téléchargement ci-dessous).</li>
-                <li>De respecter un délais de 7 jours de réflexion entre la réservation et l'adoption de l'animal.</li>
+                <li>De prendre connaissance et de signer un certificat d’engagement et de connaissance (à télécharger) regroupant les besoins physiologiques, comportementaux, médicaux, financiers de l’animal. Ainsi que l’obligation d’identification et les implications liées à la détention d’un animal.</li>
+                <li>De respecter un délai de 7 jours de réflexion entre la réservation et l’adoption de l’animal.</li>
             </ul>
 
             <a href="medias/documents/certificat-d-engagement-et-de-connaissance-chien.pdf" target="_blank" class="btn btn-outline-primary">Certificat d'engagement et de connaissance chien</a>
@@ -49,6 +36,122 @@
             <a href="medias/documents/certificat-d-engagement-et-de-connaissance-nac.pdf" target="_blank" class="btn btn-outline-primary">Certificat d'engagement et de connaissance nac</a>
 
             <br><br>
+
+            <h4>TARIFS <span style="font-weight: 300;">(à compter du 1er juillet 2024)</span></h4>
+
+            <br>
+
+            <p><u>CHIENS</u></p>
+
+            <ul>
+                <li>Chiot (mâle/femelle) : 350 euros <i>afin de réaliser les interventions obligatoires (stérilisation etc), le refuge vous donne un bon à remettre au vétérinaire</i></li>
+                <li>Mâle adulte : 300 euros</li>
+                <li>Femelle adulte : 350 euros</li>
+                <li>Mâle/femelle entre 8 et 10 ans : 150 euros</li>
+                <li>Mâle/femelle + de 10 ans : don libre</li>
+            </ul>
+
+            <p>Tous nos chiens sont stérilisés, vaccinés CHPPIL, identifiés par puce électronique, déparasités et vermifugés.</p>
+
+            <p><b><u>A savoir :</u></b> à partir de 10 ans, nos chiens bénéficient de l’<span style="color: red;"><b><i>OPERATION DOYEN</i></b></span> de 30 millions d’amis qui consiste à aider l’adoptant dans la prise en charge de futurs soins vétérinaires à hauteur de <b>800 euros</b>.</p>
+
+            <p><u>CHATS</u></p>
+
+            <ul>
+                <li>Châton mâle/femelle : 230 euros <i>fin de réaliser les interventions obligatoires (stérilisation etc), le refuge vous donne un bon à remettre au vétérinaire</i>a</li>
+                <li>Mâle adulte : 180 euros</li>
+                <li>Femelle adulte: 230 euros</li>
+                <li>Mâle/femelle + de 10 ans : don libre</li>
+            </ul>
+
+            <p>Tous nos chats sont stérilisés, vaccinés TC, testés FIV/FELV, identifiés par puce électronique, déparasités, vermifugés.</p>
+
+            <p><b><u>A savoir :</u></b> à partir de 10 ans, nos chats bénéficient de l’<span style="color: red;"><b><i>OPERATION DOYEN</i></b></span> de 30 millions d’amis qui consiste à aider l’adoptant dans la prise en charge de futurs soins vétérinaires à hauteur de <b>800 euros</b>.</p>
+
+            <p><u>NAC</u></p>
+
+            <ul>
+                <li>100€</li>
+            </ul>
+
+            <p>Tous nos nac sont stérilisés, vaccinés.</p>
+
+            <p style="color: blue;">Le refuge n’a pas valeur à vendre les animaux. Les frais d’adoption servent exclusivement à récupérer les frais vétérinaires engagés.</p>
+
+            <br>
+
+            <h4>REGLEMENT DES FRAIS D’ADOPTION</h4>
+
+            <ul>
+                <li>En liquide</li>
+                <li>Par chèque (à l’ordre de  )</li>
+                <li>Par carte bancaire</li>
+            </ul>
+
+            <p>Un reçu vous sera remis.
+            <br>Le règlement n’est effectif qu’à l’adoption définitive.</p>
+
+            <br>
+
+            <h4>MATERIEL A PREVOIR POUR LA SORTIE DE L’ANIMAL</h4>
+
+            <br>
+
+            <p><u>CHIENS</u></p>
+
+            <ul>
+                <li>Laisse</li>
+                <li>Collier/harnais</li>
+                <li>Couverture/drap pour la voiture</li>
+                <li>Système de sécurité (grille de coffre, ceinture de sécurité spécial chien, accompagnant)</li>
+            </ul>
+
+            <p><u>CHATS / NAC</u></p>
+
+            <ul>
+                <li>Caisse de transport</li>
+            </ul>
+
+            <br>
+
+            <h4>MATERIEL A PREVOIR POUR L’ARRIVEE DE L’ANIMAL</h4>
+
+            <br>
+
+            <p><u>CHIENS</u></p>
+
+            <ul>
+                <li>Endroit dans la maison au calme et sécurisé</li>
+                <li>Panier, couverture, coussin</li>
+                <li>Jouets</li>
+                <li>Activités solides et à mâcher</li>
+                <li>Gamelles nourriture/eau</li>
+                <li>Laisse, collier/harnais (muselière éventuelle), sacs à crottes</li>
+                <li>Brosse, shampooing pour chien</li>
+            </ul>
+
+            <p><u>CHATS</u></p>
+
+            <ul>
+                <li>Bac à litière</li>
+                <li>Litière + pelle à crottes</li>
+                <li>Corbeille et coussin</li>
+                <li>Griffoir</li>
+                <li>Jouets</li>
+                <li>Brosse (facultatif)</li>
+                <li>Arbre à chat (facultatif)</li>
+            </ul>
+
+            <p><u>NAC</u></p>
+
+            <ul>
+                <li>Cage aménagée et adaptée</li>
+                <li>Litière adaptée</li>
+                <li>Nourriture</li>
+                <li>Jeux</li>
+            </ul>
+
+            <br>
 
         </div>
 
--- infos-chiens-categorises.php
+++ infos-chiens-categorises.php
@@ -13,55 +13,62 @@
 
             <h1>Informations chiens catégorisés</h1><hr><br>
 
-            <h4><u>Les chiens de catégorie 1 :</u></h4>
+            <h4><u>Chiens de catégorie 1 :</u></h4>
             <ul>
-                <li>chiens de type American Staffordshire terrier (anciennement Staffordshire terrier), également appelés « pit-bulls »</li>
-                <li>chiens de type Mastiff, également appelés « boerbulls »</li>
-                <li>chiens de type Tosa.</li>
+                <li>American Staffordshire Terrier non inscrits au LOF (également appelés «Pitbulls»)</li>
+                <li>Staffordshire Bull Terrier non inscrits au LOF (également appelés «Staffies»)</li>
+                <li>Mastiff non inscrits au LOF (également appelés «Boerbulls»)</li>
+                <li>Tosa non inscrits au LOF</li>
             </ul>
 
-            <h4><u>Les chiens de catégorie 2 :</u></h4>
+            <h4><u>Chiens de catégorie 2 :</u></h4>
             <ul>
-                <li>chiens de race American Staffordshire terrier (anciennement Staffordshire terrier)</li>
-                <li>chiens de race Rottweiller</li>
-                <li>chiens de type Rottweiller</li>
-                <li>chiens de race Tosa</li>
+                <li>American Staffordshire Terrier inscrits au LOF</li>
+                <li>Tosa inscrits au LO</li>
+                <li>Rottweiller inscrits au LO</li>
+                <li>Assimilés Rottweiller non inscrits au LOF</li>
             </ul>
 
-            <h4><u>Procédure chien catégorisé :</u></h4>
+            <br>
 
-            <p>Pour les chiens de catégorie 1 l'adoptant sera le détenteur de l'animal mais pas son propriétaire, c'est à dire que l'animal vivra chez son adoptant mais il doit rester à vie au nom du refuge.<br><br>
+            <h4>PROCEDURE D’ADOPTION CHIENS CATEGORISES</h4>
 
-            Compris dans les frais d'adoption :</p>
+            <p><u>Frais d’adoption comprenant :</u></p>
             <ul>
-                <li>Identification</li>
-                <li>Vaccination de base et vaccin contre la rage</li>
-                <li>Stérilisation</li>
+                <li>Identification (obligatoire)</li>
+                <li>Vaccination CHPPIL + vaccin rage (obligatoire et <span style="color: red;">pris en charge par le refuge</span>)</li>
+                <li>Stérilisation (obligatoire)</li>
             </ul>
 
-            <p>Obligatoire avant adoption d'un chien catégorisé, aux frais de l'adoptant :</p>
+            <p><u>Obligatoire <b>AVANT</b> adoption :</u></p>
             <ul>
-                <li>Possession d'une attestation d'aptitude</li>
-                <li>Évaluation comportementale du chien</li>
-                <li>Souscription d'une assurance responsabilité civile.</li>
+                <li>Attestation d’aptitude à la détention d’un chien catégorisé (liste des formateurs agréés à la DDPP et en mairie) → environ 120 euros</li>
+                <li>Évaluation comportementale du chien auprès d’un vétérinaire agréé <span style="color: red;">→ pris en charge par le refuge</span></li>
+                <li>Assurance responsabilité civile</li>
             </ul>
 
-            <p>A faire une fois l'animal adopté :</p>
+            <p><u>Obligatoire <b>APRES</b> adoption :</u></p>
             <ul>
-                <li>Déclaration en mairie et demande d'un permis de détention</li>
-                <li>Port de la muselière obligatoire dans les lieux publiques</li>
+                <li>Déclaration en mairie (services de la Police Municipale) et demande d’un permis de détention <span style="color: red;">→ gratuit</span></li>
+                <li>Port de la muselière dans les lieux publics</li>
+                <li>Tenue des vaccins + rage à jour</li>
             </ul>
+
+            <br>
+
+            <p style="color: red;"><u><b>A savoir :</b></u> La loi n’autorise pas l’adoption des chiens de catégorie 1. C’est pourquoi nous proposons un contrat de garde à vie qui permet à nos chiens catégorisés 1 de bénéficier de la chaleur d’un foyer au même titre que les autres. L’animal reste officiellement au nom du refuge mais l’adoptant en est le détenteur et l’heureux maître et comme pour une adoption normale, tous les frais habituels (nourriture, soins etc) sont à sa charge.</p>
+
+            <br>
 
             <h4><u>Ne sont pas autorisées à détenir un chien de catégorie 1 ou 2 :</u></h4>
             <ul>
                 <li>les personnes mineures</li>
-                <li>les personnes majeures sous tutelle (sauf si autorisation par le juge des tutelles)</li>
-                <li>les personnes condamnées (crime ou peine d'emprisonnement pour délit inscrit au bulletin n°2)</li>
-                <li>les personnes auxquelles on a retiré le droit de possession ou de garde d'un chien</li>
+                <li>les personnes majeures sous tutelle (sauf autorisation du juge des tutelles)</li>
+                <li>les personnes condamnées pour crime ou peine d’emprisonnement avec ou sans sursis pour délit (inscrit au bulletin n°2 du casier judiciaire)</li>
+                <li>les personnes auxquelles ont a retiré le droit de propriété ou de garde d’un chien</li>
             </ul>
 
-
-            <p style="color: red; font-weight: 500;">Le refuge s'engage à vous fournir les informations nécessaires et vous accompagne tout au long de la procédure.</p>
+            <p style="color: blue; font-weight: 500;">Le refuge s'engage à vous fournir les informations nécessaires et vous accompagne tout au long de la procédure.</p>
 
             <br>
 
